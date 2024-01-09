f9c5f16cab761655e69f3e851786b71153dbac1f
Matheus <matheus.alves.front.m2@gmail.com>
2024-01-09 00:10:19 -0300

    just created 404 not found

---
+++ src/app/not-found.tsx
@@ -0,0 +1,10 @@
+export default function NotFoundPage() {
+  return (
+    <section style={{
+      padding: '20px'
+    }}>
+      <h1>Whoops... Sorry, this page is not available</h1>
+      <p>If something wrong please contact me.</p>
+    </section>
+  )
+}
